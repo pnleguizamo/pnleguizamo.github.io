292e08e113e57cc6402ff064b9edeb01756615cf
pnleguizamo <pnleguizamo@gmail.com>
2024-09-06 13:46:47 -0500

    Prev Tracker Work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // JavaScript code to print numbers using a for loop
 const numberList = document.getElementById("numberList");
 
-for (let i = 1; i <= 9; i++) {
+for (let i = 1; i <= 10; i++) {
     const listItem = document.createElement("li");
     listItem.textContent = i;
     numberList.appendChild(listItem);
