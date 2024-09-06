ba63b262068c6d15d3f6805e6572488d654cc9a9
pnleguizamo <pnleguizamo@gmail.com>
2024-09-06 13:50:01 -0500

    319 Activity 4

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript code to print numbers using a for loop
-const numberList = document.getElementById("numberList");
-
-for (let i = 1; i <= 10; i++) {
-    const listItem = document.createElement("li");
-    listItem.textContent = i;
-    numberList.appendChild(listItem);
-}
